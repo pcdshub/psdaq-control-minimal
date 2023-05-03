165c7c81e155e7154cffe92e21c6357cfcd940dd
Zachary Lentz <zlentz@slac.stanford.edu>
2023-05-03 15:56:43 -0700

    BLD/CI: adjust the CI workflow, simplify and modernize the build

---
+++ generate_minimal_package.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# This script clones the lcls2 daq and builds a minimized package.
+# take TAG to be the synchronized tag in this repo and in the lcls2 repo
+if [ -z "${1}" ]; then
+  TAG="$(git tag -l | tail -n 1)"
+else
+  TAG="${1}"
+fi
+git clone git@github.com:slac-lcls/lcls2.git --depth 0 --branch "${TAG}"
+mkdir psdaq
+echo "from ._version import __version__" > psdaq/__init__.py
+mkdir psdaq/control
+cp lcls2/psdaq/psdaq/control/* psdaq/control/*
